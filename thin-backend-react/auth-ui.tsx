42d03f2473e1b378b886c3866623a7ecd1de4093
Marc Scholten <marc@digitallyinduced.com>
2022-06-03 18:13:29 +0200

    Removed form variable

--- thin-backend-react/auth-ui.tsx
+++ thin-backend-react/auth-ui.tsx
@@ -87,8 +87,7 @@ function LoginForm({ loadingSpinner }) {
     const [lastError, setLastError] = useState<'UserLocked' | 'UserUnconfirmed' | 'InvalidCredentials' | null>(null);
 
     const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
-        const form = event.currentTarget;
-        const { email, password } = form;
+        const { email, password } = event.currentTarget;
 
         event.preventDefault();
         setLoading(true);
@@ -210,8 +209,7 @@ function SignUpForm({ description = 'Sign up for an account to use this applicat
     const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
         event.preventDefault();
         
-        const form = event.currentTarget;
-        const { email, password } = form;
+        const { email, password } = event.currentTarget;
 
         setLoading(true);
         setValidationFailures([]);
